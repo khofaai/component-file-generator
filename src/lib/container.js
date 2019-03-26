464055044aa671376d12151ae2f1c846f449162c
zinebzee <48124383+zinebzee@users.noreply.github.com>
2019-03-26 23:31:55 +0100

    cheking format after code review
    sorry for the delay je decouvre tjr github desktop

--- src/lib/container.js
+++ src/lib/container.js
@@ -7,7 +7,7 @@ let structureTarget = '';
 module.exports = {
 
 	promptQuestions(target) {
-		if(typeof target === 'object' && target.length > 0) {
+		if (typeof target === 'object' && target.length > 0) {
 			configurator.startCLI(componentData => {
 				this.generateCustomComponent(componentData)
 			}, target);
@@ -16,19 +16,30 @@ module.exports = {
 			structureTarget = target !== '' ? `/${target}` : '';
 			// end check
 			configurator.startCLI(componentName => {
-				this.generateComponent(componentName) 
+				componentName = this.checkformat(componentName);
+				this.generateComponent(componentName)
 			});
 		}
 	},
+
+	checkformat(componentName) {
+
+		var string = componentName.toLowerCase().split(' ');
+		for (var i = 0; i < string.length; i++) {
+			string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+		}
+		return string.join('');
+	},
+
 
 	generateComponent(componentName) {
 		let structure = require(`../config${structureTarget}/structure`);
 		let str = JSON.stringify(structure);
 		let replaceAll = (search, replacement, target) => {
-		    return target.split(search).join(replacement);
+			return target.split(search).join(replacement);
 		};
 		str = JSON.parse(replaceAll(variable, componentName, str));
-		
+
 		this.mkStructure(str, () => {
 			console.log("\x1b[42m", `${componentName} has been created successfully`, "\x1b[0m");
 		});
@@ -38,10 +49,10 @@ module.exports = {
 		let structure = componentData.body.structure;
 		let str = JSON.stringify(structure);
 		let replaceAll = (search, replacement, target) => {
-		    return target.split(search).join(replacement);
+			return target.split(search).join(replacement);
 		};
 		str = JSON.parse(replaceAll(variable, componentData.answer, str));
-		filesystem.setSource(componentData.body.root);	
+		filesystem.setSource(componentData.body.root);
 		this.mkStructure(str, () => {
 			console.log("\x1b[42m", `${componentData.answer} has been created successfully`, "\x1b[0m");
 		});
@@ -50,9 +61,9 @@ module.exports = {
 	mkStructure(structure, callback) {
 		let structureKeys = Object.keys(structure);
 		let parentPath = '';
-		if(!filesystem.checkDirectoryExistance(structure.name)) {
-			structureKeys.map( key => {
-				if (key == 'name' && typeof structure[key] === 'string' ) {
+		if (!filesystem.checkDirectoryExistance(structure.name)) {
+			structureKeys.map(key => {
+				if (key == 'name' && typeof structure[key] === 'string') {
 					filesystem.makeDirectory(structure[key]);
 					parentPath += structure[key];
 				} else {
@@ -66,9 +77,9 @@ module.exports = {
 	},
 
 	mapStructure(parentPath, structure) {
-		structure.map( _obj => {
+		structure.map(_obj => {
 			if (_obj.type == 'directory') {
-				filesystem.makeDirectory(parentPath+'/'+_obj.name);
+				filesystem.makeDirectory(parentPath + '/' + _obj.name);
 				if (typeof _obj.children !== 'undefined') {
 					this.mapStructure(parentPath + '/' + _obj.name, _obj.children);
 				}
