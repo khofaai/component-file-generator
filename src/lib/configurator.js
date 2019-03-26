8aa990337e92ffa6cc013cd62f529b379505f87b
CharkaouiMahdi <mahdi.charkaoui@gmail.com>
2019-03-26 16:19:41 +0100

    first try

--- src/lib/configurator.js
+++ src/lib/configurator.js
@@ -2,43 +2,43 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 const config = {
-	componentName : '',
+	componentName: '',
 	rl,
-	
+
 	startCLI(callback, multipleChoices = {}) {
-		if(Object.keys(multipleChoices).length > 0 ) {
+		if (Object.keys(multipleChoices).length > 0) {
 			this.runMultipleChoiceQuestion(multipleChoices, callback);
 		} else {
 			this.runQuestions(callback);
 		}
 	},
 
-	makeQuestion (question, callback) {
-  		return new Promise((resolve, reject) => {
-	    	config.rl.question(question, (answer) => {
-		    	callback(answer)
-		      	resolve();
-	    	});
-	  	});
+	makeQuestion(question, callback) {
+		return new Promise((resolve, reject) => {
+			config.rl.question(question, (answer) => {
+				callback(answer)
+				resolve();
+			});
+		});
 	},
 
 	runQuestions: async (callback) => {
-  		await config.makeQuestion("component name ? \n", answer => {
-  			config.componentName = answer;
-  		});
-  		callback(config.componentName);
-  		config.rl.close();
+		await config.makeQuestion("component name ? \n", answer => {
+			config.componentName = answer;
+		});
+		callback(config.componentName);
+		config.rl.close();
 	},
 
 	runMultipleChoiceQuestion: async (multipleChoices, callback) => {
-		
+
 		let cmdNames = 'Generate : ';
 		let selectedCommand = '';
 		let defaultCommand = [];
 		let multipleChoiceStructures = {};
 
 		await multipleChoices.map(choice => {
-			if(cmdNames != '') cmdNames += ', ';
+			if (cmdNames != '') cmdNames += ', ';
 			let key = Object.keys(choice)[0];
 			cmdNames += key;
 			defaultCommand.push(key);
@@ -47,8 +47,8 @@ const config = {
 		this.makeQuestion(`${cmdNames} ?\n`, answer => {
 			selectedCommand = answer
 		})
-		
-		if(defaultCommand,defaultCommand.includes(selectedCommand)) {
+
+		if (defaultCommand, defaultCommand.includes(selectedCommand)) {
 			this.makeQuestion(`${selectedCommand} name ?\n`, answer => {
 				config.componentName = { answer, body: multipleChoiceStructures[selectedCommand] };
 			});
@@ -58,10 +58,6 @@ const config = {
 		}
 		config.rl.close();
 	},
-
-	makeQuestion(question, callback) {
-		await config.makeQuestion(question, callback)
-	}
 }
 
 
--- src/lib/container.js
+++ src/lib/container.js
@@ -7,17 +7,48 @@ let structureOptions = '';
 
 module.exports = {
 
+	checkUpperCase(componentName) {
+		for (let i = 0; i < componentName.length; i++) {
+			if (componentName.charAt(i) === componentName.charAt(i).toUpperCase()) {
+				return true;
+			}
+		}
+		return false;
+	},
+
+	CaptalizeFirstLetter(componentName) {
+		if (componentName === componentName.toLowerCase()) {
+			console.log("1");
+			return componentName.charAt(0).toUpperCase() + componentName.slice(1);
+		} else if (componentName === componentName.toUpperCase()) {
+			console.log("2");
+			let str = componentName.charAt(0);
+			for (let i = 1; i < componentName.length; i++) {
+				str = str + componentName.charAt(i).toLowerCase();
+			}
+			return str;
+		}
+		else if (componentName.charAt(0) === componentName.charAt(0).toUpperCase()) {
+			console.log("3");
+			return componentName;
+		}
+		else if (this.checkUpperCase(componentName)) {
+			console.log("4");
+			return componentName;
+		}
+
+	},
 	promptQuestions(target) {
-		if(typeof target === 'object' && target.length > 0) {
+		if (typeof target === 'object' && target.length > 0) {
 			configurator.startCLI(componentData => {
 				this.generateCustomComponent(componentData)
 			}, target);
 		} else {
-			// check name format
+
 			structureTarget = target !== '' ? `/${target}` : '';
-			// end check
 			configurator.startCLI(componentName => {
-				this.generateComponent(componentName) 
+				componentName = this.CaptalizeFirstLetter(componentName);
+				this.generateComponent(componentName)
 			});
 		}
 	},
@@ -26,10 +57,10 @@ module.exports = {
 		let structure = require(`../config${structureTarget}/structure`);
 		let str = JSON.stringify(structure);
 		let replaceAll = (search, replacement, target) => {
-		    return target.split(search).join(replacement);
+			return target.split(search).join(replacement);
 		};
 		str = JSON.parse(replaceAll(variable, componentName, str));
-		
+
 		this.mkStructure(str, () => {
 			console.log("\x1b[42m", `${componentName} has been created successfully`, "\x1b[0m");
 		});
@@ -39,10 +70,10 @@ module.exports = {
 		let structure = componentData.body.structure;
 		let str = JSON.stringify(structure);
 		let replaceAll = (search, replacement, target) => {
-		    return target.split(search).join(replacement);
+			return target.split(search).join(replacement);
 		};
 		str = JSON.parse(replaceAll(variable, componentData.answer, str));
-		filesystem.setSource(componentData.body.root);	
+		filesystem.setSource(componentData.body.root);
 		this.mkStructure(str, () => {
 			console.log("\x1b[42m", `${componentData.answer} has been created successfully`, "\x1b[0m");
 		});
@@ -51,9 +82,9 @@ module.exports = {
 	mkStructure(structure, callback) {
 		let structureKeys = Object.keys(structure);
 		let parentPath = '';
-		if(!filesystem.checkDirectoryExistance(structure.name)) {
-			structureKeys.map( key => {
-				if (key == 'name' && typeof structure[key] === 'string' ) {
+		if (!filesystem.checkDirectoryExistance(structure.name)) {
+			structureKeys.map(key => {
+				if (key == 'name' && typeof structure[key] === 'string') {
 					filesystem.makeDirectory(structure[key]);
 					parentPath += structure[key];
 				} else {
@@ -67,9 +98,9 @@ module.exports = {
 	},
 
 	mapStructure(parentPath, structure) {
-		structure.map( _obj => {
+		structure.map(_obj => {
 			if (_obj.type == 'directory') {
-				filesystem.makeDirectory(parentPath+'/'+_obj.name);
+				filesystem.makeDirectory(parentPath + '/' + _obj.name);
 				if (typeof _obj.children !== 'undefined') {
 					this.mapStructure(parentPath + '/' + _obj.name, _obj.children);
 				}
